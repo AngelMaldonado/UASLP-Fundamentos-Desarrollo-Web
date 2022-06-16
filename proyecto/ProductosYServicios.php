8d252bf0ae92c8c54d99dd5e6b28becb0f2799f4
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-06-15 22:41:54 -0500

    🎨 Implementación del diseño #6: ✅ Refactorización y diseño responsivo en Productos y Servicios

---
+++ proyecto/ProductosYServicios.php
@@ -0,0 +1,267 @@
+<?php require('./views/layouts/header.php') ?>
+
+<main>
+    <section class="portada">
+        <div class="portada__titulo">
+            <h1>Productos y servicios</h1>
+        </div>
+    </section>
+    <nav class="barraBusqueda">
+        <ul class="barraBusqueda__categorias">
+            <li><a class="etiqueta -primario" href="/">Paquetes de graduación</a></li>
+            <li><a class="etiqueta -primario" href="/">Anillos</a></li>
+            <li><a class="etiqueta -primario" href="/">Artículos promocionales</a></li>
+            <li><a class="etiqueta -primario" href="/">Trofeos y reconocimientos</a></li>
+            <li><a class="etiqueta -primario" href="/">Renta de togas y birretes</a></li>
+        </ul>
+        <div class="cajaTexto -chico">
+            <input type="search" placeholder="Buscar">
+            <a href="">
+                <?php echo file_get_contents("./assets/svg/buscar.svg"); ?>
+            </a>
+        </div>
+    </nav>
+    <section class="tarjetasProductos">
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+        <!-- tarjetaProducto -->
+        <li class="tarjetaProducto">
+            <header class="tarjetaProducto__titulo">
+                <h1>Paquete de graduación diamante</h1>
+            </header>
+            <div class="tarjetaProducto__caracteristicas">
+                <h2>$1850.00</h2>
+                <ul class="controlEstrellas">
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                    <li class="controlEstrellas__estrella">
+                        <?php echo file_get_contents("./assets/svg/estrella.svg"); ?>
+                    </li>
+                </ul>
+                <a class="boton -secundario -mediano" href="/">Ver detalles</a>
+            </div>
+        </li>
+        <!-- fin tarjetaProducto -->
+    </section>
+    <nav class="paginas">
+        <a class="enlace -primario -grande" href="/">
+            <?php echo file_get_contents("./assets/svg/diamante-izquierda.svg"); ?>
+            Anterior
+        </a>
+        <ul>
+            <li><a class="enlace -primario" href="/">1</a></li>
+            <li><a class="enlace -primario" href="/">2</a></li>
+            <li><a class="enlace -primario" href="/">3</a></li>
+        </ul>
+        <a class="enlace -primario -grande" href="/">
+            Siguiente
+            <?php echo file_get_contents("./assets/svg/diamante-derecha.svg"); ?>
+        </a>
+    </nav>
+</main>
+
+<?php require('./views/layouts/footer.php') ?>
