99833e8dd3a853b0d7e651f3a1d116c656c5c998
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-06-21 08:48:36 -0500

    🎨 Implementación del diseño #6: ❇️ Pedidos/paquetes, Inventario, Usuarios (LIMPIAR)

---
+++ proyecto/views/admin/Usuarios.php
@@ -0,0 +1,17 @@
+<?php require('../layouts/dashboard_header.php'); ?>
+
+<main>
+    <section>
+        <h1>Usuarios</h1>
+        <div class="tarjetasUsuario">
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+            <?php require('../components/tarjetaUsuario.php'); ?>
+        </div>
+    </section>
+</main>
