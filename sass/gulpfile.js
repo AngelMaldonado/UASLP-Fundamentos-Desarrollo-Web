b14af77acd961a0f1f0e46cab149e02614420557
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-07-14 12:08:15 -0500

    🎒Festival - Dependencias Gulp para optimización de imágenes y sus funciones en paralelo

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -3,9 +3,17 @@
  *  Created On : Mon Jul 11 2022
  *  File : gulpfile.js
  *******************************************/
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+// Imagenes
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const avif = require("gulp-avif");
+const webp = require("gulp-webp");
 
 function css(done) {
 
@@ -20,10 +28,49 @@ function css(done) {
     done();
 }
 
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
+function versionWebp(done) {
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    };
+
+    src("src/img/**/*.{png,jpg}")
+        .pipe(avif(opciones))
+        .pipe(dest("build/img"));
+
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.imagenes = imagenes;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
