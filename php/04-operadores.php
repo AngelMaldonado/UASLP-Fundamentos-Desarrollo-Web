aa5f3b38367b38f467b9d18e6b2c0838d074d8a8
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-07-26 13:52:19 -0500

    🎒PHP - Operadores

---
+++ php/04-operadores.php
@@ -0,0 +1,22 @@
+<?php include 'includes/header.php';
+
+$numero1 = 10;
+$numero2 = 20;
+
+// Sumar
+echo $numero1 + $numero2;
+
+// Restar
+echo $numero1 - $numero2;
+echo "<br>";
+
+// Multiplicar
+echo $numero1 * $numero2;
+
+// Dividir
+echo $numero2 / $numero1;
+
+// Multiplicar cierta cantidad de veces
+echo 2 ** 3;
+
+include 'includes/footer.php';
